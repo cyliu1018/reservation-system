eaf22adbeafb7ef549a400006fe2148f4ee051c3
chihyen <nlsh710599@gmail.com>
2023-10-21 11:55:16 +0800

    add config service and swagger service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  const configService = app.get(ConfigService);
+
+  if (Boolean(configService.get<string>('SWAGGER_ENABLED', '0'))) {
+    const config = new DocumentBuilder()
+      .setTitle('Reservation System')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('api', app, document);
+  }
+
+  await app.listen(Number(configService.get<string>('PORT', '3000')));
 }
 bootstrap();
