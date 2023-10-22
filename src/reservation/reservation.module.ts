e882c24a48e607abfb8c62df9007b11c6aa2e312
chihyen <nlsh710599@gmail.com>
2023-10-22 18:05:59 +0800

    implement insertReservationRecordWithInviteCode function

--- src/reservation/reservation.module.ts
+++ src/reservation/reservation.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService, ConfigModule } from '@nestjs/config';
 
 import { ReservationService } from './reservation.service';
 import { ReservationController } from './reservation.controller';
 import { Reservation } from '../db';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Reservation])],
-  providers: [ReservationService],
+  imports: [TypeOrmModule.forFeature([Reservation]), ConfigModule],
+  providers: [ReservationService, ConfigService],
   exports: [ReservationService],
   controllers: [ReservationController],
 })
--- src/reservation/reservation.service.ts
+++ src/reservation/reservation.service.ts
@@ -4,16 +4,21 @@ import {
   BadRequestException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 import { Repository } from 'typeorm';
 
 import { Reservation } from '../db';
-import { isCodeValid } from '../util/inviteCode';
+import { isCodeValid, generateCode } from '../util/inviteCode';
 
 @Injectable()
 export class ReservationService {
+  private LIMIT_COUNT_FOR_USER_RESERVE_BY_MOCA_NFT: string;
+  private LIMIT_COUNT_FOR_USER_RESERVE_BY_INVITE_CODE: string;
+
   constructor(
     @InjectRepository(Reservation)
     private reservationRepository: Repository<Reservation>,
+    private configService: ConfigService,
   ) {
     this.reservationRepository.upsert(
       {
@@ -23,6 +28,19 @@ export class ReservationService {
       },
       ['email'],
     );
+    try {
+      this.LIMIT_COUNT_FOR_USER_RESERVE_BY_INVITE_CODE =
+        this.configService.getOrThrow<string>(
+          'LIMIT_COUNT_FOR_USER_RESERVE_BY_INVITE_CODE',
+        );
+
+      this.LIMIT_COUNT_FOR_USER_RESERVE_BY_MOCA_NFT =
+        this.configService.getOrThrow<string>(
+          'LIMIT_COUNT_FOR_USER_RESERVE_BY_MOCA_NFT',
+        );
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   hello(name: string): string {
@@ -60,13 +78,37 @@ export class ReservationService {
         return new BadRequestException('invalid invite code');
       }
 
-      const res = await this.reservationRepository.findOneBy({ email });
+      let res = await this.reservationRepository.findOneBy({ email });
 
       if (res) {
         return new BadRequestException('User already exist');
       }
 
-      return;
+      try {
+        res = (
+          await this.reservationRepository.query(
+            `INSERT INTO reservation (email, referrer, invite_code, reserve_by_moca_nft) SELECT $1, $2, $3, $4 FROM reservation HAVING ((SELECT COUNT(*) FROM reservation WHERE referrer = $5) < $6 AND (SELECT reserve_by_moca_nft FROM reservation WHERE invite_code = $7) = true OR (SELECT COUNT(*) FROM reservation WHERE referrer = $8) < $9 AND (SELECT reserve_by_moca_nft FROM reservation WHERE invite_code = $10) = false) AND (SELECT COUNT(*) FROM reservation WHERE invite_code = $11) = 1 RETURNING *;`,
+            [
+              email,
+              inviteCode,
+              generateCode(),
+              false,
+              inviteCode,
+              this.LIMIT_COUNT_FOR_USER_RESERVE_BY_MOCA_NFT,
+              inviteCode,
+              inviteCode,
+              this.LIMIT_COUNT_FOR_USER_RESERVE_BY_INVITE_CODE,
+              inviteCode,
+              inviteCode,
+            ],
+          )
+        )[0];
+      } catch (error) {
+        return new InternalServerErrorException();
+      }
+      return res
+        ? res
+        : new BadRequestException('Invite code usage limit reached');
     } catch (error) {
       return new InternalServerErrorException();
     }
